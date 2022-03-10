d0d68a19483027cf805a320a2dc1ede93c75263d
safin777 <sayedislamsafin7770555@gmail.com>
2022-03-11 01:37:29 +0600

    React component added

---
+++ src/Components/Basic.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic = ({locale}) => {
+  return (
+    <div>
+      <h2>Now the time is:</h2>
+      <h4>Hello {new Date().toLocaleTimeString(locale)}</h4>
+    </div>
+  )
+}
+
+export default Basic
