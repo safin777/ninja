d1375f5cbb6620830f125ae4bf2344405bb84170
safin777 <sayedislamsafin7770555@gmail.com>
2022-03-31 19:41:52 +0600

    play with select and text area in event

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -5,21 +5,56 @@ export default class Form extends Component {
 
   state = {
     name : '',
+    description : '',
+    notes:  '',
   }
 
   handleTitle = (e) =>{
-     console.log (e.target.value)
-     this.setState ({
-       name: e.target.value,
-     });
+    
+     if(e.target.type === 'text'){  
+      console.log (e.target.value);
+      this.setState({
+        name: e.target.value
+      })
+     }
+
+     else if(e.target.type === 'textarea'){
+      console.log (e.target.value);
+      this.setState({
+        description: e.target.value
+      })
+     }
+
+     else if(e.target.type === 'select-one'){
+      
+      this.setState({
+        notes: e.target.value
+      })
+     }
+
+     else{
+      console.log('Nothing is here');
+     }
+      
+     
   }
+
+  
   
 
   render() {
-    const {name} = this.state;
+    const {name,description,notes} = this.state;
     return (
-      <div >
+      <div>
         <input type="text" value={name} onChange={this.handleTitle} placeholder='Enter your username'/>
+        <br></br>
+        <input type="textarea" value={description} onChange={this.handleTitle}/>
+        <br></br>
+        <select value={notes} onChange={this.handleTitle}>
+          <option value="Notes 1">Notes 1</option>
+          <option value="Notes 2">Notes 2</option>
+          <option value="Notes 3">Notes 3</option>
+        </select>
         
       </div>
     )
